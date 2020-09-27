77288f69c688a699a002a698c867836f4be99a14
burhanuguz <59168275+burhanuguz@users.noreply.github.com>
2020-09-27 21:43:44 +0300

    Update Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -10,7 +10,7 @@ namespace HelloWeb
         {
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Hello Hepsiburada from Burhan Uguz!!!");
+                return context.Response.WriteAsync("Hello World");
             });
         }
     }
